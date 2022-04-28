5ffeca41dc0c0f582428ab6a11917c443b0c0038
EClemMarq <erik.clemens@marquette.edu>
2022-04-28 11:00:12 -0500

    Fix error

--- runFBPConvNet.py
+++ runFBPConvNet.py
@@ -205,7 +205,7 @@ def main(options, seed = 0):
     # If the user wants to add the model graph and sample images to Tensorboard
     if options.graph:
         if options.trainVal:
-            tr_low_fbp, tr_full_fbp = iter(tr_loader).next()
+            low_fbp, full_fbp = iter(tr_loader).next()
         else:
             low_fbp, full_fbp = iter(test_loader).next()
             
